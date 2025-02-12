d7ee0b2da30fddd87bf427716106251988c1bc0c
villainouskirby <69393156+villainouskirby@users.noreply.github.com>
2025-02-12 21:07:51 +0900

    오류뜨는거 없애고 푸시

--- Assets/Scripts/UI/SliderHandler.cs
+++ Assets/Scripts/UI/SliderHandler.cs
@@ -15,7 +15,6 @@ public class SliderHandler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        _bgmSlider.onValueChanged += ChangeBgm;
 
     }
 
