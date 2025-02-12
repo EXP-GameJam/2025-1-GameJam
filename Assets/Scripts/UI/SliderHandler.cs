223d6b13bbe6c717611970b5db98a5489c6512a0
villainouskirby <69393156+villainouskirby@users.noreply.github.com>
2025-02-12 21:20:26 +0900

    슬라이더 UI

--- Assets/Scripts/UI/SliderHandler.cs
+++ Assets/Scripts/UI/SliderHandler.cs
@@ -15,21 +15,40 @@ public class SliderHandler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        _bgmSlider.onValueChanged.AddListener(ChangeBgm);
+        _sfxSlider.onValueChanged.AddListener(ChangeSfx);
+        _sensitivitySlider.onValueChanged.AddListener(ChangeSensitivity);
 
+        InitSlider();
     }
 
-    private void ChangeBgm()
+    private void InitSlider()
     {
-
+        _bgmSlider.value = PlayerPrefs.GetFloat("BGM", 1);
+        _sfxSlider.value = PlayerPrefs.GetFloat("SFX", 1);
+        _sensitivitySlider.value = PlayerPrefs.GetFloat("Sensitivity", 1);
     }
 
-    private void ChangeSfx()
+    private void ChangeBgm(float value)
     {
-
+        audioMixer.SetFloat("BGM", GetVolume(value));
+        PlayerPrefs.SetFloat("BGM", value);
+        PlayerPrefs.Save();
     }
 
-    private void ChangeSensitivity()
+    private void ChangeSfx(float value)
     {
-
+        audioMixer.SetFloat("SFX", GetVolume(value));
+        PlayerPrefs.SetFloat("SFX", value);
+        PlayerPrefs.Save();
     }
+
+    private void ChangeSensitivity(float value)
+    {
+        audioMixer.SetFloat("Sensitivity", value);
+        PlayerPrefs.SetFloat("Sensitivity", value);
+        PlayerPrefs.Save();
+    }
+
+    private float GetVolume(float value) => Mathf.Log10(value) * 20;
 }
