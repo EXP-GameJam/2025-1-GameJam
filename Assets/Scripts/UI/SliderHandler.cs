916b2e63bf38362a7ce1acaf28da78544f476233
villainouskirby <69393156+villainouskirby@users.noreply.github.com>
2025-02-12 21:07:22 +0900

    로켓 프리팹

--- Assets/Scripts/UI/SliderHandler.cs
+++ Assets/Scripts/UI/SliderHandler.cs
@@ -1,18 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SliderHandler : MonoBehaviour
 {
+    [SerializeField] private AudioMixer audioMixer;
+
+    [SerializeField] private Slider _bgmSlider;
+    [SerializeField] private Slider _sfxSlider;
+    [SerializeField] private Slider _sensitivitySlider;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _bgmSlider.onValueChanged += ChangeBgm;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void ChangeBgm()
     {
-        
+
+    }
+
+    private void ChangeSfx()
+    {
+
+    }
+
+    private void ChangeSensitivity()
+    {
+
     }
 }
